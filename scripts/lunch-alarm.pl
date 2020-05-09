6c49c8980ff72894e2b233919f6919d696811079
KDr2 <zhuo@hexoasis.com>
2020-05-09 08:38:55 +0800

    lunch alarm: more restaurants

--- scripts/lunch-alarm.pl
+++ scripts/lunch-alarm.pl
@@ -35,7 +35,7 @@ my $payload = {
                      pretext      => "来自 Culinarian 的提醒",
                      title        => "该吃午饭啦！",
                      title_link   => "",
-                     text         => "@all 如果不知道吃什么，何不看看下面的今日推荐：",
+                     text         => q/@all 如果不知道吃什么，何不看看下面的今日推荐：/,
                      fields       => [ map { _restaurant $_ } @lines[0..2] ],
                  }]
 };
@@ -57,3 +57,7 @@ __DATA__
 昶丰臭桂鱼@https://www.meituan.com/meishi/175478934/
 唐潮记潮汕砂锅粥@https://www.meituan.com/meishi/179126665/
 赵小姐创意中餐@https://www.meituan.com/meishi/159237534/
+西汉年间@https://www.meituan.com/meishi/179759805/
+饺子公馆@https://www.meituan.com/meishi/180564025/
+赵家腊汁肉@https://www.meituan.com/meishi/179756401/
+炒炒任小米@https://www.meituan.com/meishi/195938245/
